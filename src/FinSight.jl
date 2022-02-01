9ac4b4ac8585a3b7366caf5aead88a28d6044b24
Zhanibek <zhanibek.om@gmail.com>
2022-02-01 14:36:18 +0900

    added xirr and xnpv functions

--- src/FinSight.jl
+++ src/FinSight.jl
@@ -1,12 +1,15 @@
 module FinSight
-
-# Write your package code here.
+using Roots: find_zero
+using Dates
 
 """
 Calculates the net present value of an investment by using a discount rate and a
 series of future payments (negative values) and income (positive values).
-Implentation from
-https://support.microsoft.com/en-us/office/npv-function-8672cb67-2576-4d07-b67b-ac28acf2a568
+
+rate - the rate of discount over the length of one period.
+values - arguments representing the payments and income.
+
+From https://support.microsoft.com/en-us/office/npv-function-8672cb67-2576-4d07-b67b-ac28acf2a568
 """
 function npv(rate::Real, values::AbstractVector{T}) where T <:Real
     @assert length(values) > 0
@@ -17,5 +20,54 @@ function npv(rate::Real, values::AbstractVector{T}) where T <:Real
     return out
 end
 export npv
+
+"""
+Returns the internal rate of return for a series of cash flows represented by
+the numbers in values. These cash flows do not have to be even, as they would be
+for an annuity.
+
+values - An array or a reference to cells that contain numbers for
+which you want to calculate the internal rate of return.
+rate_guess -  A number that you guess is close to the result of IRR
+
+From https://support.microsoft.com/en-us/office/irr-function-64925eaa-9988-495b-b290-3ad0c163c1bc
+"""
+function irr(values::AbstractVector{T}, rate_guess::Real = 0.1) where T <:Real
+    @assert length(values) > 0
+    rate = find_zero(x -> npv(x, values),  rate_guess, )
+    return rate
+end
+export irr
+
+"""
+Returns the net present value for a schedule of cash flows that is not
+necessarily periodic. To calculate the net present value for a series of cash
+flows that is periodic, use the NPV function.
+"""
+function xnpv(rate::Real, values::AbstractVector{T}, dates::Vector{DateTime}) where T <:Real
+    @assert length(values) > 0
+    @assert length(values) == length(dates)
+    out = 0
+    for (i, val) in enumerate(values)
+        exponent = Day(dates[i] - first(dates)) / Day(365)
+        out += val / (1 + rate)^exponent
+    end
+    return out
+end
+export xnpv
+
+"""
+Returns the internal rate of return for a schedule of cash flows that is not
+necessarily periodic. To calculate the internal rate of return for a series of
+periodic cash flows, use the IRR function.
+"""
+function xirr(values::AbstractVector{T}, dates::Vector{DateTime}, rate_guess::Real = 0.1) where T <:Real
+    @assert length(values) > 0
+    @assert length(values) == length(dates)
+    rate = find_zero(x -> xnpv(x, values, dates),  rate_guess)
+    return rate
+end
+export xirr
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,24 @@
 using FinSight
+using Dates
 using Test
-
-@testset "FinSight.jl" begin
+@testset "(x)irr and (x)npv functions" begin
 # Examples from
 # https://support.microsoft.com/en-us/office/npv-function-8672cb67-2576-4d07-b67b-ac28acf2a568
     @test isapprox(npv(0.1, [-10000, 3000, 4200, 6800]), 1188.44, rtol = 1e-3)
     @test isapprox(npv(0.08, [8000, 9200, 10000, 12000, 14500]) + (-40000), 1922.06, rtol=1e-3)
+
+    let values = [-70e3, 12e3, 15e3, 18e3, 21e3, 26e3]
+        @test isapprox(irr(values[1:end-1]), -0.021, atol=1e-3)
+        @test isapprox(npv(irr(values), values), 0.0, atol=1e-6)
+        v2 = @view values[1:5]
+        @test isapprox(npv(irr(v2), v2), 0.0, atol=1e-6)
+    end
+
+    # From https://support.microsoft.com/en-us/office/xnpv-function-1b42bbf6-370f-4532-a0eb-d67c16b664b7
+    let (dates, values) =
+        ([DateTime(2008, 1, 1), DateTime(2008, 3, 1), DateTime(2008, 10, 30), DateTime(2009,2,15), DateTime(2009,4, 1)],
+         [-10e3, 2750, 4250, 3250, 2750])
+        @test isapprox(xnpv(0.09, values, dates), 2086.65, atol=1e-2)
+        @test isapprox(xnpv(xirr(values, dates), values, dates), 0.0, atol=1e-6)
+    end
 end
