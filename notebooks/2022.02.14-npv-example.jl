f430d7c1f1624d6d8385bd88d7da422991a83faa
Zhanibek <zhanibek.om@gmail.com>
2022-02-15 02:41:54 +0900

    updated example

--- notebooks/2022.02.14-npv-example.jl
+++ notebooks/2022.02.14-npv-example.jl
@@ -4,140 +4,107 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ d6cfceae-779d-474d-a609-e1fe6fb1d276
+begin
+	import Pkg
+	Pkg.activate(".")
+end
+
+# ╔═╡ ea6b61fa-51d1-499a-884a-b7fba187d005
+begin
+	using FinSight
+	using Plots
+	using PlutoUI
+	using DataFrames
+	using Measurements
+end
+
 # ╔═╡ c2f43cb6-e226-4be6-b54c-96417ae953fa
 # Illistrative "pro forma" DCF valuation of a rental property
 
-# ╔═╡ 138676f7-18b8-4f1e-b4e2-b14bb0c0c6f0
-#  Assumptions 
+# ╔═╡ d7e8f31d-86e9-4832-878b-be2d925767dc
+begin
+	# rate
+	r = 0.07
+	
+	# Vacancy 5.0% of gross income
+	v = 0.05
+	
+	# Capital Expenditures 10% of gross income 
+	ci = 0.10 ± 0  # Could be zero
+	
+	#Growth Rate 2%
+	g = 0.02 ± 0.001
+	
+	# Expenses 35% of gross income
+	expense_ratio = 0.35 ± 0.05
+	
+	# Exit Capitalisation Rate 5%
+	y = 0.05
+	
+	# Time horizon now (year 0) plus 11 years
+	year = [0,0,0, 0,0,0, 0,0,0, 0, 0,0]  
+	
+	# Potential Gross Income first year $100
+	pgi_fy = 100.0 ± 20
 
-# Discount Rate 7%
-r = 0.07 
-
-# ╔═╡ 61a0f4ed-d089-469d-b710-ec120db396b8
-# Vacancy 5.0% of gross income
-v = 0.05
-
-# ╔═╡ 13f86375-41e5-4363-b8cc-1b0b8a8bf6ae
-# Capital Expenditures 10% of gross income 
-ci = 0.10
-
-# ╔═╡ 5a3840cb-506c-4951-8d6d-af52d19b76df
-#Growth Rate 2%
-g = 0.02
-
-# ╔═╡ d522f1fe-3e3a-4d92-b873-8d6b5a438d7b
-# Expenses 35% of gross income
-expense_ratio = 0.35
-
-# ╔═╡ c0155310-6b8f-491f-94da-590ce2a121c0
-# Exit Capitalisation Rate 5%
-y = 0.05
-
-# ╔═╡ dd69dbc8-f4a6-4f5c-b417-e4e359199580
-# Time horizon now (year 0) plus 11 years
-year = [0,0,0, 0,0,0, 0,0,0, 0, 0,0]  
-
-# ╔═╡ 1baf4382-8caa-4582-8d6f-e2d387b7d775
-# Potential Gross Income first year $100
-pgi_fy = 100.0
+	nothing
+end
 
 # ╔═╡ 6d70da11-1f37-46c1-bae2-c9635674f4e2
 # Potential Gross Income nominal cash flow 
 begin
-	pgi =  [values = 0.0 for values = 0:11  ]  # create an income array years 0-11 
-	pgi[2] = pgi_fy  # first year gross income is the second value
+	pgi = zeros(typeof(pgi_fy), 12)
+	pgi[2] = pgi_fy   # first year gross income is the second value
 	growth_factor = 1.0 + g  
 	
 	for i = 3:12
 			prior_year_income = pgi[i-1]
 	 		pgi[i] =   prior_year_income * growth_factor  		
 	end
+
+	pgi
 end
 
 # ╔═╡ 06a77d3e-e321-446a-9e18-8a53e3ad2214
 pgi  # Potential gross income per square metre of office space
 
-# ╔═╡ 758105e9-2277-442f-96e7-4b88f6cbaea4
-# Vacancy allowance
-vacancy_allowance = pgi .* v
+# ╔═╡ 46f48d40-e8d6-4b4a-8176-06ae91298209
+begin
+	df = DataFrame();
+	df.years = 1:length(year)
+	df.pgi = pgi
+	df.vacancy_allowance = df.pgi .* v
+	df.effective_gross_income = df.pgi - df.vacancy_allowance
+	df.operating_expenses = expense_ratio * df.pgi
+	df.net_operating_income = df.effective_gross_income - df.operating_expenses
+	df.capital_expenditures = ci * df.pgi
+	df.net_annual_cash_flows = df.net_operating_income - df.capital_expenditures
+	df.reversion_cash_flows = [0, 0,0,0,0,0, 0,0,0,0, df.net_annual_cash_flows[12] / y ,0]
 
-# ╔═╡ 8c15b2eb-d3d8-4a6e-9af4-5dea167bb9ef
-# Effective gross income
-effective_gross_income = pgi - vacancy_allowance
+	df.net_cash_flows = df.net_annual_cash_flows + df.reversion_cash_flows
 
-# ╔═╡ 978c98da-8d3e-4a46-8d0d-b1fc309765e3
-# Operating expenses
-operating_expenses = expense_ratio * pgi
-
-# ╔═╡ fa6e76de-4f87-4e28-90ec-96a7e050af92
-# Net operating income
-net_operating_income = effective_gross_income - operating_expenses
-
-# ╔═╡ 682ab75d-cec1-4072-a1c5-2fcb2c0723cf
-# Capital expenditures
-capital_expenditures = ci * pgi
-
-# ╔═╡ d5e03d54-8a61-48d9-8d82-ebc890225bf6
-# Net annual cash flows
-net_annual_cash_flows = net_operating_income - capital_expenditures
-
-# ╔═╡ c70deae0-92a7-42e0-85eb-f46c38a9c7a7
-# Reversion cash flows
-reversion_cash_flows = [0, 0,0,0,0,0, 0,0,0,0, net_annual_cash_flows[12] / y ,0] 
-
-# ╔═╡ de488ff6-f6d1-4bd5-8042-6a0f525c9352
-# Net cash flows
-net_cash_flows = net_annual_cash_flows + reversion_cash_flows
-
-# ╔═╡ a15ed659-66bd-48e9-9501-1b8729672caf
-function npv(rate::Real, values::AbstractVector{T}) where T <:Real
-    out = 0
-    for (i, val) in enumerate(values)
-        out += val / (1 + rate)^i
-    end
-    return out
+	df
 end
 
 # ╔═╡ 7e23378a-5293-4e9a-b308-89324cd1cad2
 # Present value @ 7.0% Discount rate
-net_present_value = round( npv(r, net_cash_flows[2:11]) )
+net_present_value = round(npv(r, df.net_cash_flows[2:11]))
 
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-"""
+# ╔═╡ 772f35f4-5d5c-4359-a14c-d88e704bee49
+plot(df.years, df.net_cash_flows, xlab="years", ylab="Net cash flows")
 
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
+# ╔═╡ b2286e8c-f911-457c-ab86-66799f380f70
 
-julia_version = "1.7.0"
-manifest_format = "2.0"
-
-[deps]
-"""
 
 # ╔═╡ Cell order:
+# ╠═d6cfceae-779d-474d-a609-e1fe6fb1d276
+# ╠═ea6b61fa-51d1-499a-884a-b7fba187d005
 # ╠═c2f43cb6-e226-4be6-b54c-96417ae953fa
-# ╠═138676f7-18b8-4f1e-b4e2-b14bb0c0c6f0
-# ╠═61a0f4ed-d089-469d-b710-ec120db396b8
-# ╠═13f86375-41e5-4363-b8cc-1b0b8a8bf6ae
-# ╠═5a3840cb-506c-4951-8d6d-af52d19b76df
-# ╠═d522f1fe-3e3a-4d92-b873-8d6b5a438d7b
-# ╠═c0155310-6b8f-491f-94da-590ce2a121c0
-# ╠═dd69dbc8-f4a6-4f5c-b417-e4e359199580
-# ╠═1baf4382-8caa-4582-8d6f-e2d387b7d775
+# ╠═d7e8f31d-86e9-4832-878b-be2d925767dc
 # ╠═6d70da11-1f37-46c1-bae2-c9635674f4e2
 # ╠═06a77d3e-e321-446a-9e18-8a53e3ad2214
-# ╠═758105e9-2277-442f-96e7-4b88f6cbaea4
-# ╠═8c15b2eb-d3d8-4a6e-9af4-5dea167bb9ef
-# ╠═978c98da-8d3e-4a46-8d0d-b1fc309765e3
-# ╠═fa6e76de-4f87-4e28-90ec-96a7e050af92
-# ╠═682ab75d-cec1-4072-a1c5-2fcb2c0723cf
-# ╠═d5e03d54-8a61-48d9-8d82-ebc890225bf6
-# ╠═c70deae0-92a7-42e0-85eb-f46c38a9c7a7
-# ╠═de488ff6-f6d1-4bd5-8042-6a0f525c9352
-# ╠═a15ed659-66bd-48e9-9501-1b8729672caf
+# ╠═46f48d40-e8d6-4b4a-8176-06ae91298209
 # ╠═7e23378a-5293-4e9a-b308-89324cd1cad2
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
+# ╠═772f35f4-5d5c-4359-a14c-d88e704bee49
+# ╠═b2286e8c-f911-457c-ab86-66799f380f70
